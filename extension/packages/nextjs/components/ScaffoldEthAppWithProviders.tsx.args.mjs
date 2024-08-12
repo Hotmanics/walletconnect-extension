b668a05b9ca8418972a172d5141174e0594bb525
Jacob Homanics <homanicsjake@gmail.com>
2024-08-12 17:52:29 -0500

    Update ScaffoldEthAppWithProviders.tsx.args.mjs

--- extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
+++ extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
@@ -1,5 +1,5 @@
 export const providerImports = `import { createWeb3Modal } from "@web3modal/wagmi/react"; import { metadata, projectId, walletConnectWagmiConfig } from "~~/services/web3/walletConnectWagmiConfig";`;
 export const walletProviderOpeningTags = "";
 export const walletProviderClosingTags = "";
-// export const preSetup = "createWeb3Modal({metadata, wagmiConfig: walletConnectWagmiConfig, projectId, enableAnalytics: true, // Optional - defaults to your Cloud configuration});"
+export const preSetup = "createWeb3Modal({metadata, wagmiConfig: walletConnectWagmiConfig, projectId, enableAnalytics: true});"
 export const wagmiConfig = "walletConnectWagmiConfig";
